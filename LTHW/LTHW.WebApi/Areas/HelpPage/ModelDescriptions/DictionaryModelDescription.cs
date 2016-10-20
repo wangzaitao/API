996aabeb019fa89d03e635371935bd40c0c515c1
Guantonghua <guantonghua@163.com>
2016-10-20 10:48:40 +0800

    delete

--- LTHW/LTHW.WebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace LTHW.WebApi.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- LTHW/LTHW.WebApi/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LTHW.WebApi.WebApiApplication" Language="C#" %>
